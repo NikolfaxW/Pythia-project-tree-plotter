1e5ec499bd8b2160c44533198c2a0262e7629035
NikolfaxW <denisnik@cvut.cz>
2024-03-19 12:41:42 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "Pythia8/Pythia.h"
+#include "TCanvas.h"
+#include "TString.h"
+#include "TH2D.h"
+#include "TMath.h"
+#include "TMarker.h"
+#include "TRandom3.h"
+#include "TStyle.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "fastjet/PseudoJet.hh"
+#include "fastjet/ClusterSequence.hh"
+
+
+int main() {
+
+    return 0;
+}
